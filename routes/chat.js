a4d14ff0acb3236c475b3aa884010c5c94c79375
TelmaMercado <ceinnova02132@innovaccion.mx>
2025-08-01 23:01:26 -0600

    Update the architecture of the chatbot, give the correct context

---
+++ routes/chat.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getChatResponse } from "../services/openaiService.js";
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const reply = await getChatResponse(message);
+    res.json({ reply });
+  } catch (error) {
+    console.error("Error en /chat:", error.message);
+    res.status(500).json({ error: "Error al procesar la solicitud" });
+  }
+});
+
+export default router;
---
+++ services/openaiServices.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import fs from "fs/promises"; // 👈 usamos Promesas
+import path from "path";
+
+
+export async function getChatResponse(message) {
+    const contextPath = path.resolve("context", "context.txt");
+    const systemPrompt = await fs.readFile(contextPath, "utf-8");
+    
+  const response = await axios.post(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:  systemPrompt
+        },
+        {
+          role: "user",
+          content: message
+        }
+      ]
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  return response.data.choices[0].message.content;
+}
